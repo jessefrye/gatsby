0e429f2e2774e932bdc1ea5d32f89595ff1b56bd
Jesse <teamlinx@gmail.com>
2018-09-08 02:08:21 -0700

    eslint nits

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const NotFoundPage = () => (
   <div>
     <h1>404</h1>
     <p>I think you broke the internet</p>
   </div>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
